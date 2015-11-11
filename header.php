afbd493191e61d8f1dcc0f968264113e661c5c4f
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-11-11 16:19:36 +0100

    header.php – head-Element mit entsprechenden WordPress-Funktionen

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<?php if ( is_singular() && pings_open( get_queried_object() ) ) { ?>
+	<?php if ( is_singular() && pings_open() ) { ?>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php }
 	wp_head(); ?>
