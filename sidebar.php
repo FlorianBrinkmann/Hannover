8ec70586c9ee50311e599810eb6ee1945307f77f
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-11-18 17:24:43 +0100

    sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<?php if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
+	<aside id="secondary" role="complementary">
+		<h2 class="screen-reader-text">
+			<?php _ex( 'Sidebar', 'screen reader text for the sidebar', 'hannover' ) ?></h2>
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</aside>
+<?php }
