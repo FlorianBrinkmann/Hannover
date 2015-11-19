e15bad72fe7ef63679203391f4b56922af40f998
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-11-19 14:28:00 +0100

    index.php – screen-reader-headline

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php get_header(); ?>
 	<main role="main">
 		<?php if ( have_posts() ) {
+			if ( is_home() && ! is_front_page() ) { ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+			<?php }
 			while ( have_posts() ) {
 				the_post();
 				get_template_part( 'content', get_post_format() );
