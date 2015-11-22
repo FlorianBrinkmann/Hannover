053001924237e6793c78f81cbc84f92c87fd0bd0
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-11-22 12:20:23 +0100

    single.php

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 	<main role="main">
 		<?php while ( have_posts() ) {
 			the_post();
-			get_template_part( 'template-parts/content', 'single' );
+			get_template_part( 'template-parts/content-single', get_post_format() );
 			the_post_navigation();
 			if ( comments_open() || get_comments_number() ) {
 				comments_template( '', true );
