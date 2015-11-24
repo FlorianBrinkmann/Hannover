f3511625bfe14a3d8f40a7435a7f0398de7e151f
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-11-24 21:11:43 +0100

    customizer.php – archive einstellungen

--- inc/customizer.php
+++ inc/customizer.php
@@ -55,6 +55,61 @@ function hannover_customize_register( $wp_customize ) {
 		)
 	);
 
+	$wp_customize->add_setting(
+		'portfolio_archive', array(
+			'default'           => 'no_archive',
+			'sanitize_callback' => 'hannover_sanitize_select'
+		)
+	);
+
+	$wp_customize->add_control(
+		'portfolio_archive', array(
+			'label'    => __( 'Choose how you want to select portfolio elements for the archive.', 'hannover' ),
+			'type'     => 'radio',
+			'section'  => 'portfolio_archive',
+			'settings' => 'portfolio_archive',
+			'choices'  => array(
+				'no_archive'       => 'No archive',
+				'archive_category' => 'Archive category',
+				'auto_archive'     => 'Automatic archive less recent elements'
+			)
+		)
+	);
+
+	$wp_customize->add_setting(
+		'portfolio_archive_category', array(
+			'sanitize_callback' => 'hannover_sanitize_select'
+		)
+	);
+
+	$wp_customize->add_control(
+		'portfolio_archive_category', array(
+			'label'           => __( 'Archive category', 'hannover' ),
+			'type'            => 'select',
+			'section'         => 'portfolio_archive',
+			'settings'        => 'portfolio_archive_category',
+			'choices'         => $category_array,
+			'active_callback' => 'hannover_choice_callback'
+		)
+	);
+
+	$wp_customize->add_setting(
+		'portfolio_auto_archive', array(
+			'default'           => 3,
+			'sanitize_callback' => 'hannover_sanitize_positive_int'
+		)
+	);
+
+	$wp_customize->add_control(
+		'portfolio_auto_archive', array(
+			'label'           => __( 'Number of recent elements which will NOT be archived.', 'hannover' ),
+			'type'            => 'number',
+			'section'         => 'portfolio_archive',
+			'settings'        => 'portfolio_auto_archive',
+			'active_callback' => 'hannover_choice_callback'
+		)
+	);
+
 	$wp_customize->add_panel(
 		'theme_options', array(
 			'title' => __( 'Theme Options', 'hannover' ),
@@ -64,6 +119,13 @@ function hannover_customize_register( $wp_customize ) {
 	$wp_customize->add_section(
 		'portfolio_elements', array(
 			'title' => __( 'Portfolio elements', 'hannover' ),
+			'panel' => 'theme_options'
+		)
+	);
+
+	$wp_customize->add_section(
+		'portfolio_archive', array(
+			'title' => __( 'Portfolio archive', 'hannover' ),
 			'panel' => 'theme_options'
 		)
 	);
@@ -79,6 +141,19 @@ function hannover_use_portfolio_category_callback( $control ) {
 	}
 }
 
+function hannover_choice_callback( $control ) {
+	$radio_setting = $control->manager->get_setting( 'portfolio_archive' )->value();
+	$control_id    = $control->id;
+	if ( $control_id == 'portfolio_archive_category' && $radio_setting == 'archive_category' ) {
+		return true;
+	}
+	if ( $control_id == 'portfolio_auto_archive' && $radio_setting == 'auto_archive' ) {
+		return true;
+	}
+
+	return false;
+}
+
 function hannover_sanitize_select( $input, $setting ) {
 	$input   = sanitize_key( $input );
 	$choices = $setting->manager->get_control( $setting->id )->choices;
@@ -88,4 +163,10 @@ function hannover_sanitize_select( $input, $setting ) {
 
 function hannover_sanitize_checkbox( $checked ) {
 	return ( ( isset( $checked ) && true == $checked ) ? true : false );
+}
+
+function hannover_sanitize_positive_int( $number, $setting ) {
+	$number = absint( $number );
+
+	return ( $number > 0 ? $number : $setting->default );
 }
