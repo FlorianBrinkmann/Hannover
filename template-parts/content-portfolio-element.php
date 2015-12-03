36d0ef1590d5d2f42e25a3ed18190c19561b872b
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-12-03 17:11:54 +0100

    content-portfolio-element.php angepasst

--- template-parts/content-portfolio-element.php
+++ template-parts/content-portfolio-element.php
@@ -1,3 +1,6 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-	<?php hannover_image_from_gallery_or_image_post( 'thumbnail', $post ); ?>
+	<a href="<?php the_permalink(); ?>">
+		<h2 class="screen-reader-text"><?php the_title(); ?></h2>
+		<?php hannover_image_from_gallery_or_image_post( 'thumbnail', $post ); ?>
+	</a>
 </article>
