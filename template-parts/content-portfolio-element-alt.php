de7b45b335ed1f7f6d132201e1fae3bc4da1d13b
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-12-11 15:04:14 +0100

    content-portfolio-element-alt.php

--- template-parts/content-portfolio-element-alt.php
+++ template-parts/content-portfolio-element-alt.php
@@ -1,5 +1,13 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 	<a href="<?php the_permalink(); ?>">
-		<?php hannover_image_from_gallery_or_image_post( 'thumbnail', $post ); ?>
+		<?php hannover_image_from_gallery_or_image_post( 'medium_large', $post ); ?>
 	</a>
+	<div>
+		<a href="<?php the_permalink(); ?>">
+			<h2><?php the_title(); ?></h2>
+		</a>
+		<?php if ( has_excerpt() ) {
+			the_excerpt();
+		} ?>
+	</div>
 </article>
