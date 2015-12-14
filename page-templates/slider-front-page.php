987ad59508c8afe28f9d1c7edebf702f3bb41e21
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-12-14 15:53:46 +0100

    slider-front-page.php

---
+++ page-templates/slider-front-page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Front page with slider
+ */
+get_header(); ?>
+	<main role="main">
+		<?php
+		while ( have_posts() ) {
+			the_post();
+			get_template_part( 'template-parts/content', 'page' );
+			if ( comments_open() || get_comments_number() ) {
+				comments_template( '', true );
+			}
+		} ?>
+	</main>
+<?php get_footer();
