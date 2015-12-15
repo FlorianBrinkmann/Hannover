3a29d543a2126e59950c8fb356d2eba85d1ebe0a
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-12-15 18:04:52 +0100

    content-gallery.php und content-image.php

---
+++ template-parts/content-gallery.php
@@ -0,0 +1,16 @@
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+	<header class="entry-header">
+		<?php hannover_image_from_gallery_or_image_post( 'large', $post );
+		hannover_the_title( 'h2', true ); ?>
+	</header>
+	<div class="entry-content">
+		<?php if ( has_excerpt() ) {
+			the_excerpt();
+		} ?>
+	</div>
+	<footer>
+		<p><a href="<?php the_permalink(); ?>"
+		      class="entry-date"><?php hannover_the_date(); ?></a>
+			<?php hannover_entry_meta() ?></p>
+	</footer>
+</article>
