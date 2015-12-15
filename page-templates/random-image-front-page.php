f2bf70884430b2c2a500cc8a01d4c26375e37b6e
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-12-15 12:26:02 +0100

    random-image-front-page.php

---
+++ page-templates/random-image-front-page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: Front page with random image
+ */
+get_header(); ?>
+	<main role="main">
+		<?php $images = hannover_get_gallery_images( $post->ID );
+		shuffle( $images );
+		$first_image = array_shift( $images ); ?>
+		<figure>
+			<?php echo wp_get_attachment_image( $first_image->ID, 'large' );
+			if ( ! empty( $first_image->post_title ) ) { ?>
+				<figcaption>
+					<?php echo $first_image->post_title; ?>
+				</figcaption>
+			<?php } ?>
+		</figure>
+	</main>
+<?php get_footer();
