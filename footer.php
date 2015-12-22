641779fdf1985115eb4d798c6eb3740109f5a311
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-12-22 16:36:51 +0100

    footer.php – Social-Media- und Footer-Menü sowie Autoren-Hinweis

--- footer.php
+++ footer.php
@@ -1,4 +1,32 @@
 </div>
+<footer id="footer">
+	<?php if ( has_nav_menu( 'social' ) ) {
+		wp_nav_menu(
+			array(
+				'theme_location' => 'social',
+				'menu_class'     => 'social-menu',
+				'container'      => '',
+				'walker'         => new Hannover_Social_Menu_Walker(),
+				'depth'          => 1
+			)
+		);
+	}
+	if ( has_nav_menu( 'footer' ) ) { ?>
+		<nav>
+			<h2 class="screen-reader-text">
+				<?php _ex( 'Footer navigation', 'hidden screen reader headline for the footer navigation', 'hannover' ); ?>
+			</h2>
+			<?php wp_nav_menu(
+				array(
+					'theme_location' => 'footer',
+					'menu_class'     => 'footer-menu',
+					'container'      => ''
+				)
+			); ?>
+		</nav>
+	<?php } ?>
+	<p class="theme-author"><?php _e( 'Theme: Hannover by <a rel="nofollow" href="https://florianbrinkmann.de">Florian Brinkmann</a>', 'bornholm' ) ?></p>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
