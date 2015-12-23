4d77604e22397d3fbdd0d75d0a0b19f221837ada
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-12-23 17:43:08 +0100

    single.php – kleine weitere Anpassung

--- single.php
+++ single.php
@@ -1,6 +1,7 @@
 <?php get_header();
-$use_portfolio_category = get_theme_mod( 'portfolio_from_category' );
-$portfolio_post         = false;
+$use_portfolio_category     = get_theme_mod( 'portfolio_from_category' );
+$exclude_portfolio_elements = get_theme_mod( 'exclude_portfolio_elements_from_blog' );
+$portfolio_post             = false;
 if ( $use_portfolio_category == 'checked' ) {
 	$portfolio_category = get_theme_mod( 'portfolio_category' );
 	if ( has_category( $portfolio_category, $post ) ) {
@@ -17,6 +18,9 @@ if ( $use_portfolio_category == 'checked' ) {
 			the_post();
 			if ( $portfolio_post ) {
 				get_template_part( 'template-parts/content-single-portfolio' );
+				if ( $exclude_portfolio_elements != 'checked' ) {
+					the_post_navigation();
+				}
 			} else {
 				get_template_part( 'template-parts/content-single', get_post_format() );
 				the_post_navigation();
