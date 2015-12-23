63b5e81093e404df2bde710767d71abec7f1bae5
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-12-23 17:40:02 +0100

    single.php – Behandlung von Portfolio-Elementen

--- single.php
+++ single.php
@@ -1,13 +1,32 @@
-<?php get_header(); ?>
+<?php get_header();
+$use_portfolio_category = get_theme_mod( 'portfolio_from_category' );
+$portfolio_post         = false;
+if ( $use_portfolio_category == 'checked' ) {
+	$portfolio_category = get_theme_mod( 'portfolio_category' );
+	if ( has_category( $portfolio_category, $post ) ) {
+		$portfolio_post = true;
+	}
+} else {
+	$format = get_post_format( $post_id );
+	if ( $format == 'gallery' || $format == 'image' ) {
+		$portfolio_post = true;
+	}
+} ?>
 	<main role="main">
 		<?php while ( have_posts() ) {
 			the_post();
-			get_template_part( 'template-parts/content-single', get_post_format() );
-			the_post_navigation();
+			if ( $portfolio_post ) {
+				get_template_part( 'template-parts/content-single-portfolio' );
+			} else {
+				get_template_part( 'template-parts/content-single', get_post_format() );
+				the_post_navigation();
+			}
 			if ( comments_open() || get_comments_number() ) {
 				comments_template( '', true );
 			}
 		} ?>
 	</main>
-<?php get_sidebar();
+<?php if ( ! $portfolio_post ) {
+	get_sidebar();
+}
 get_footer();
