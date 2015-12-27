cd9d6553930b9412a8d876192f57ba0afde11776
Florian Brinkmann <kontakt@florianbrinkmann.de>
2015-12-27 12:23:35 +0100

    search.php

---
+++ search.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+	<main role="main">
+		<?php if ( have_posts() ) { ?>
+			<header class="page-header">
+				<h1><?php printf( __( 'Search Results for: %s', 'hannover' ), esc_html( get_search_query() ) ); ?></h1>
+			</header>
+			<?php while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/content', get_post_format() );
+			}
+		} else { ?>
+			<header class="page-header">
+				<h1><?php printf( __( 'Nothing found for “%s”', 'hannover' ), esc_html( get_search_query() ) ); ?></h1>
+			</header>
+			<?php get_search_form();
+		}
+		the_posts_pagination( array( 'type' => 'list' ) ); ?>
+	</main>
+<?php get_sidebar();
+get_footer();
