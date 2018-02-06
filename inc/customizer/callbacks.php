a8b704b7abc03b93f3aace30a1dab8229e6e0529
Florian Brinkmann <kontakt@florianbrinkmann.com>
2018-02-06 11:50:57 +0100

    Callback functions

--- inc/customizer/callbacks.php
+++ inc/customizer/callbacks.php
@@ -8,73 +8,83 @@
  */
 
 /**
- * Checks if checkbox for portfolio category is checked and returns true, otherwise false
+ * Checkbox sanitization callback example.
  *
- * @param $control
+ * Sanitization callback for 'checkbox' type controls. This callback sanitizes
+ * `$checked` as a boolean value, either TRUE or FALSE.
  *
- * @return bool
- */
-function hannover_use_portfolio_category_callback( $control ) {
-	if ( $control->manager->get_setting( 'portfolio_from_category' )->value() == 'checked' ) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-function hannover_header_textcolor() {
-	if ( get_theme_mod( 'header_textcolor' ) ) { ?>
-		<style type="text/css">
-			#header,
-			#header a {
-				color: <?php echo '#' . get_theme_mod('header_textcolor'); ?>;
-			}
-		</style>
-	<?php }
-}
-
-add_action( 'wp_head', 'hannover_header_textcolor' );
-
-/**
- * Callback function for controls dependent from radio controls
+ * @link https://github.com/WPTRT/code-examples/blob/master/customizer/sanitization-callbacks.php
  *
- * @param $control
+ * @param bool $checked Whether the checkbox is checked.
  *
- * @return bool
- */
-function hannover_choice_callback( $control ) {
-	$radio_setting = $control->manager->get_setting( 'portfolio_archive' )->value();
-	$control_id    = $control->id;
-	if ( $control_id == 'portfolio_archive_category' && $radio_setting == 'archive_category' ) {
-		return true;
-	} elseif ( $control_id == 'portfolio_archive_remove_category_from_cat_widget' && $radio_setting == 'archive_category' ) {
-		return true;
-	}
-
-	return false;
-}
-
-/**
- * Sanitizes select input
- *
- * @param $input , $setting
- *
- * @return string
- */
-function hannover_sanitize_select( $input, $setting ) {
-	$input   = sanitize_key( $input );
-	$choices = $setting->manager->get_control( $setting->id )->choices;
-
-	return ( array_key_exists( $input, $choices ) ? $input : $setting->default );
-}
-
-/**
- * Sanitizes checkbox input
- *
- * @param $checked
- *
- * @return bool
+ * @return bool Whether the checkbox is checked.
  */
 function hannover_sanitize_checkbox( $checked ) {
-	return ( ( isset( $checked ) && true == $checked ) ? true : false );
+	return ( ( isset( $checked ) && true === $checked ) ? true : false );
+}
+
+/**
+ * Drop-down Pages sanitization callback example.
+ *
+ * - Sanitization: dropdown-pages
+ * - Control: dropdown-pages
+ *
+ * Sanitization callback for 'dropdown-pages' type controls. This callback
+ * sanitizes `$page_id` as an absolute integer, and then validates that $input
+ * is the ID of a published page.
+ *
+ * @see  absint() https://developer.wordpress.org/reference/functions/absint/
+ * @see  get_post_status()
+ *       https://developer.wordpress.org/reference/functions/get_post_status/
+ *
+ * @link https://github.com/WPTRT/code-examples/blob/master/customizer/sanitization-callbacks.php
+ *
+ * @param int                  $page_id Page ID.
+ * @param WP_Customize_Setting $setting Setting instance.
+ *
+ * @return int|string Page ID if the page is published; otherwise, the setting
+ *                    default.
+ */
+function hannover_sanitize_dropdown_pages( $page_id, $setting ) {
+	// Ensure $input is an absolute integer.
+	$page_id = absint( $page_id );
+
+	// If $page_id is an ID of a published page, return it; otherwise, return the default.
+	return ( 'publish' === get_post_status( $page_id ) ? $page_id : $setting->default );
+}
+
+
+/**
+ * Select sanitization callback example.
+ *
+ * - Sanitization: select
+ * - Control: select, radio
+ *
+ * Sanitization callback for 'select' and 'radio' type controls. This callback
+ * sanitizes `$input` as a slug, and then validates `$input` against the
+ * choices defined for the control.
+ *
+ * @see  sanitize_key()
+ *       https://developer.wordpress.org/reference/functions/sanitize_key/
+ * @see  $wp_customize->get_control()
+ *       https://developer.wordpress.org/reference/classes/wp_customize_manager/get_control/
+ *
+ * @link https://github.com/WPTRT/code-examples/blob/master/customizer/sanitization-callbacks.php#L21
+ *
+ * @param string               $input   Slug to sanitize.
+ * @param WP_Customize_Setting $setting Setting instance.
+ *
+ * @return string Sanitized slug if it is a valid choice; otherwise, the
+ *                setting default.
+ */
+function hannover_sanitize_select( $input, $setting ) {
+
+	// Ensure input is a slug.
+	$input = sanitize_key( $input );
+
+	// Get list of choices from the control associated with the setting.
+	$choices = $setting->manager->get_control( $setting->id )->choices;
+
+	// If the input is a valid key, return it; otherwise, return the default.
+	return ( array_key_exists( $input, $choices ) ? $input : $setting->default );
 }
