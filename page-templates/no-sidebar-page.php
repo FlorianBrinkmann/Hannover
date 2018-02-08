314c900c72be2bc942e043123479aaa240aff884
Florian Brinkmann <kontakt@florianbrinkmann.com>
2018-02-08 10:17:23 +0100

    Reworked no-sidebar-page.php

--- page-templates/no-sidebar-page.php
+++ page-templates/no-sidebar-page.php
@@ -2,18 +2,34 @@
 /**
  * Template Name: Page without Sidebar
  *
- * @version 1.0.4
+ * @version 2.0.0
+ *
+ * @package Hannover
  */
+
+// Include header.php.
 get_header(); ?>
-	<main role="main">
-		<?php if ( have_posts() ) {
+	<main>
+		<?php // Check if we have posts.
+		if ( have_posts() ) {
+			// Loop the posts.
 			while ( have_posts() ) {
+				// Setup post.
 				the_post();
+
+				// Include content-page.php.
 				get_template_part( 'template-parts/content', 'page' );
+
+				// Check if comments are open or we already have comments
+				// and include comments.php.
 				if ( comments_open() || get_comments_number() ) {
-					comments_template( '', true );
-				}
-			}
-		} ?>
+					comments_template();
+				} // End if().
+			} // End while().
+		} else {
+			// We do not have posts, so we include the content-none.php.
+			get_template_part( 'template-parts/content', 'none' );
+		} // End if(). ?>
 	</main>
-<?php get_footer();
+<?php // Include footer.php.
+get_footer();
